b4986cc2a3b121b500e4366bcf0da15d59dc36ec
Joseph Soukup <joseph.soukup@outlook.com>
2020-08-12 15:37:02 +0200

    finished visualization

---
+++ boston_dataset.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from computing import *
+from sklearn.datasets import load_boston
+
+dataset = load_boston()
+df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+
+np.random.seed(42)
+
+
+x = dataset.data[:, 6]
+y = dataset.target
+
+indices = np.random.permutation(len(x))
+test_size =100
+
+x_train = x[indices[:-test_size]]
+y_train = y[indices[:-test_size]]
+
+x_test = x[indices[-test_size:]]
+y_test = y[indices[-test_size:]]
+
+
+def train_model(x, y):
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    m = computing.compute_slope(x, y, x_mean, y_mean)
+    b = computing.compute_intercept(x_mean, y_mean, m)
+    print("Training is finished")
+    print("With Params m: %s and b: %d" % (m, b))
+    regression_line = computing.compute_regression(x, m, b)
+    r2 = computing.compute_r2(y, y_mean, regression_line)
+    print("With R2: %s" % r2)
+    return m, b
+def test_model(x, y, m, b):
+    y_mean = np.mean(y)
+    regression_line = computing.compute_regression(x, m, b)
+    r2 = computing.compute_r2(y, y_mean, regression_line)
+    print("Finished testing")
+    print("With R2: %s" % r2)
+    return r2
+
+m, b = train_model(x_train, y_train)
+r2_test = test_model(x_test, y_test, m, b)
+
+LB = int(np.floor(np.min(x))) -5
+UB = int(np.floor(np.max(x))) +5
+
+line = [m * i + b for i in [LB, UB]]
+
+plt.scatter(x_test, y_test, color="blue")
+plt.plot([LB, UB], line, color="red")
+plt.show()
---
+++ computing.py
@@ -0,0 +1,23 @@
+class computing:
+    def __init__(self):
+       pass 
+    def compute_slope( x, y, x_mean, y_mean):
+        frac1 = sum([(x[i] - x_mean) * (y[i] - y_mean) for i in range(len(x))])
+        frac2 = sum([(x[i] - x_mean)**2 for i in range(len(x))])
+        slope = frac1 / frac2
+        return slope
+
+    def compute_intercept(x_mean, y_mean, slope):
+        intercept = y_mean - slope * x_mean
+        return intercept
+
+    def compute_regression(x, slope, intercept):
+        regression_line = [slope * x[i] + intercept for i in range(len(x))]
+        return regression_line
+    
+    def compute_r2(y, y_mean, regression_line):
+        frac1 = sum([(y[i] - regression_line[i])**2 for i in range(len(y))])
+        frac2 = sum([(y[i] - y_mean)**2 for i in range(len(y))])
+        r2 = 1 - frac1 / frac2
+        return r2
+
